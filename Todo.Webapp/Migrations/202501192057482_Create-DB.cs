c85c529ca108b0163b47e929375c6d2c57369673
Samyar <samyar1380.work@gmail.com>
2025-01-20 00:47:36 +0330

    Add project files.

---
+++ Todo.Webapp/Migrations/202501192057482_Create-DB.cs
@@ -0,0 +1,27 @@
+﻿namespace Todo.Webapp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tasks",
+                c => new
+                    {
+                        TaskId = c.Int(nullable: false, identity: true),
+                        Title = c.String(maxLength: 100),
+                        Complete = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.TaskId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Tasks");
+        }
+    }
+}
---
+++ Todo.Webapp/Models/Tasks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Todo.Webapp.Models
+{
+    public class Tasks
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [DefaultValue(false)]
+        public bool Complete { get; set; }
+    }
+}
